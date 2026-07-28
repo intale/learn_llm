f27b3cfdd37436eb6121d2317b35f0d22d8b2a13
Ivan Dzyzenko <intale.a@gmail.com>
2026-07-28 07:16:44 +0200

    Attempt to fix sitemap

--- site/sitemap.config.mjs
+++ site/sitemap.config.mjs
@@ -124,7 +124,14 @@ export function renderSitemapXml(routes, siteUrl) {
 
   return [
     '<?xml version="1.0" encoding="UTF-8"?>',
-    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+    '<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"\n' +
+    '        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"\n' +
+    '        xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"\n' +
+    '        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"\n' +
+    '        xmlns:video="http://www.google.com/schemas/sitemap-video/1.1"\n' +
+    '        xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"\n' +
+    '        xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0"\n' +
+    '        xmlns:pagemap="http://www.google.com/schemas/sitemap-pagemap/1.0" xmlns:xhtml="http://www.w3.org/1999/xhtml">',
     ...entries,
     '</urlset>',
     '',
